3dc8d0129e3f61bc782cf7d028a6ad5dab8982fa
Jasonsanjaya <jasonsanjaya1503@gmail.com>
2026-01-11 18:52:15 +0700

    merata kirikan update

--- lib/screen/table/table_detail_screen.dart
+++ lib/screen/table/table_detail_screen.dart
@@ -60,44 +60,52 @@ class _TableDetailScreenState extends State<TableDetailScreen> {
   }
 
   void _calculateColumnWidths(List<DataGridRow> rows) {
-  final Map<String, double> widths = {};
+    final Map<String, double> widths = {};
 
-  for (final col in columns) {
-    final name = col['name'];
-    double maxWidth = 80; // minimum width
+    for (final col in columns) {
+      final name = col['name'];
+      double maxWidth = 80; // minimum width
 
-    // ukur header
-    final headerPainter = TextPainter(
-      text: TextSpan(text: name, style: const TextStyle(fontWeight: FontWeight.bold)),
-      textDirection: TextDirection.ltr,
-    )..layout();
-    maxWidth = headerPainter.width + 32;
-
-    // ukur isi (ambil max dari baris yang tampil)
-    for (final row in rows) {
-      final cell = row.getCells().firstWhere((c) => c.columnName == name);
-      final text = cell.value.toString();
-
-      final painter = TextPainter(
-        text: TextSpan(text: text, style: const TextStyle(fontSize: 14)),
+      // ukur header
+      final headerPainter = TextPainter(
+        text: TextSpan(
+          text: name,
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
         textDirection: TextDirection.ltr,
-        maxLines: 1,
       )..layout();
+      maxWidth = headerPainter.width + 32;
 
-      maxWidth = painter.width + 32 > maxWidth
-          ? painter.width + 32
-          : maxWidth;
+      // ukur isi (ambil max dari baris yang tampil)
+      for (final row in rows) {
+        final cell = row.getCells().firstWhere((c) => c.columnName == name);
+        final text = cell.value.toString();
+
+        final painter = TextPainter(
+          text: TextSpan(text: text, style: const TextStyle(fontSize: 14)),
+          textDirection: TextDirection.ltr,
+          maxLines: 1,
+        )..layout();
+
+        maxWidth = painter.width + 32 > maxWidth
+            ? painter.width + 32
+            : maxWidth;
+      }
+
+      // batasi supaya tidak terlalu lebar
+      // widths[name] = maxWidth.clamp(80, 300);
+
+      if (name.toUpperCase() == 'UPDATE') {
+        widths[name] = maxWidth.clamp(80, 100); //ubah lebar kolom UPDATE   
+      } else {
+        widths[name] = maxWidth.clamp(20, 300);
+      }
     }
 
-    // batasi supaya tidak terlalu lebar
-    widths[name] = maxWidth.clamp(80, 300);
+    setState(() {
+      columnWidths = widths;
+    });
   }
-
-  setState(() {
-    columnWidths = widths;
-  });
-}
-
 
   // ================= INIT =================
   Future<void> _init() async {
@@ -200,9 +208,10 @@ class _TableDetailScreenState extends State<TableDetailScreen> {
           ? const Center(child: CircularProgressIndicator())
           : SfDataGrid(
               source: dataSource,
+              headerRowHeight: 32,
               columnWidthMode:
-                  ColumnWidthMode.lastColumnFill, //ubah lebar di dalam kotak
-              rowHeight: 50, //tinggi per kotak di dalam tabel
+                  ColumnWidthMode.fitByColumnName, //ubah lebar di dalam kotak
+              rowHeight: 40, //tinggi per kotak di dalam tabel
               // loadMoreViewBuilder: (context, loadMoreRows) {
               //   return FutureBuilder(
               //     future: loadMoreRows(),
@@ -216,15 +225,20 @@ class _TableDetailScreenState extends State<TableDetailScreen> {
               // }, //uncomment ini jika ingin menampilkan loading di bawah saat load more
               columns: columns.map((c) {
                 final name = c['name'];
-
                 return GridColumn(
                   columnName: name,
-                  width: columnWidths[name] ?? 120, //auto width 
+                  width: columnWidths[name] ?? 120, //auto width
                   label: Padding(
-                    padding: const EdgeInsets.all(8),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 2,
+                      horizontal: 6,
+                    ),
                     child: Text(
                       name,
-                      style: const TextStyle(fontWeight: FontWeight.bold),
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                      ),
                     ),
                   ),
                 );
@@ -234,7 +248,7 @@ class _TableDetailScreenState extends State<TableDetailScreen> {
       bottomNavigationBar: isSearching
           ? null
           : Container(
-              padding: const EdgeInsets.symmetric(vertical: 6),
+              padding: const EdgeInsets.symmetric(vertical: 0),
               color: const Color(0xFF0E5A6F),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -394,38 +408,87 @@ class TableGridSource extends DataGridSource {
               'DEC',
             ];
             value =
-                '${date.day.toString().padLeft(2, '0')}-${months[date.month - 1]}-${date.year}';
+                '${date.day.toString().padLeft(2, '0')} '
+                '${months[date.month - 1]} '
+                '${date.year}';
           } catch (_) {
             // kalau gagal parse, biarkan value asli
           }
         }
 
+        // ================= FORMAT HET (1.000.000) =================
+        if (column == 'HET') {
+          final num? number = num.tryParse(
+            value.replaceAll(',', '').replaceAll('.', ''),
+          );
+          if (number != null) {
+            value = _formatRupiah(number.toInt());
+          }
+        }
+
         // ===== ALIGNMENT =====
-        final isRightAlign = column == 'HET' || column == 'UPDATE';
+        final isRightAlign = column == 'HET' ;
+
+        
 
         //biar supersede tidak turun baris
         final isSupersede = column == 'SUPERSEDE';
 
         return Padding(
           padding: const EdgeInsets.all(8),
-          child: isSupersede ? 
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Text(
-              value,
-              style: const TextStyle(fontSize: 14),
-            ),
-          )
-          : Text(
-            value,
-            textAlign: isRightAlign ? TextAlign.right : TextAlign.left,
-            maxLines: 3,
-            softWrap: true,
-            overflow: TextOverflow.fade,
-            style: const TextStyle(fontSize: 14),
-          ), //ubah ukuran tabel
+          child: isSupersede
+              ? SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Text(value, style: const TextStyle(fontSize: 14)),
+                )
+                : Container(
+    alignment: isRightAlign
+        ? Alignment.centerRight
+        : Alignment.centerLeft,
+    child: Text(
+      value,
+      maxLines: 3,
+      softWrap: true,
+      overflow: TextOverflow.fade,
+      style: const TextStyle(fontSize: 14),
+    ),
+  ),
+
+              // : Align(
+              //     alignment:
+              //         isRightAlign ? Alignment.centerRight : Alignment.centerLeft,
+              //     child: Text(
+              //       value,
+              //       maxLines: 3,
+              //       softWrap: true,
+              //       overflow: TextOverflow.fade,
+              //       style: const TextStyle(fontSize: 14),
+              //     ),
+              //   ),
+              // : Text(
+              //     value,
+              //     textAlign: isRightAlign ? TextAlign.right : TextAlign.left,
+              //     maxLines: 3,
+              //     softWrap: true,
+              //     overflow: TextOverflow.fade,
+              //     style: const TextStyle(fontSize: 14),
+              //   ), //ubah ukuran tabel
         );
       }).toList(),
     );
   }
+}
+
+String _formatRupiah(int value) {
+  final s = value.toString();
+  final buffer = StringBuffer();
+
+  for (int i = 0; i < s.length; i++) {
+    buffer.write(s[s.length - 1 - i]);
+    if ((i + 1) % 3 == 0 && i + 1 != s.length) {
+      buffer.write('.');
+    }
+  }
+
+  return buffer.toString().split('').reversed.join();
 }
